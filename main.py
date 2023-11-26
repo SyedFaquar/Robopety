d3e78598733eaee9cfaafbc0459d4ddd85a3c63a
graydeec <graydeec21@gmail.com>
2023-11-26 15:45:36 -0500

    filter, return, and select

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import datetime
 
-from flask import Flask, redirect, url_for, request, render_template, session
-from db import db
+from flask import Flask, redirect, url_for, request, render_template, session, jsonify
+from db import db, get_db_pet
 
 app = Flask(__name__)
 
 ## home page for robopety
 @app.route("/")
 def root():
-    return render_template("login.html")
+    return redirect(url_for('login'))
 
 ## login page
 @app.route('/login', methods=('GET', 'POST'))
@@ -17,13 +17,13 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
-        if email is None or email != db()['email'] or  password != db()['password']:
+        if email is None or email != db()['email'] or password != db()['password']:
             error = 'You have entered an incorrect email or password.'
         
         if error is None:
             ## session.clear()
             session['username'] = email
-            return redirect(url_for('user'))
+            return redirect(url_for('user',id=1))
         
         ##flash(error)
     return render_template('login.html', error = error)    
@@ -52,20 +52,67 @@ def signup():
                 error = 'Passwords are not matching.'
             else:
                 session['username'] = email
-                return redirect(url_for('user'))
+                return redirect(url_for('user', id=1))
         ##flash(error) 
     return render_template('signup.html', error = error)
 
 ## user page
-@app.route('/user', methods=('GET', 'POST'))
-def user():
+@app.route('/user/<id>', methods=('GET', 'POST'))
+def user(id):
     username = session.get('username', None)
-    return render_template('user.html', username=username)
+    if not username:
+        return redirect(url_for('login'))
+    pets = get_db_pet()
+    currentPet = session.get('current_pet', None)
+    if currentPet:
+        pets = [item for item in pets if currentPet['name'] != item['name']]
+    return render_template('user.html', username=username, currentPet=currentPet, pets=pets)
+
+@app.route('/setpet', methods=['POST'])
+def setPet():
+    username = session.get('username', None)
+    currentPet = session.get('current_pet', None)
+    if currentPet:
+        pets = [item for item in get_db_pet() if item['name'] != currentPet['name']]
+        return render_template('user.html', username=username, currentPet=currentPet, pets=pets)
+    currentPetName = request.form.get('data_from_button')
+    pets = []
+    for item in get_db_pet():
+        if item['name'] != currentPetName:
+            pets.append(item)
+        else:
+            currentPet = item
+            session['current_pet'] = item
+    print(pets, currentPet)
+    return render_template('user.html', username=username, currentPet=currentPet, pets=pets)
+
+## filter pets
+@app.route('/filtered/<filter_value>')
+def filter_list(filter_value):
+    pets = get_db_pet()
+    currentPet = session.get('current_pet', None)
+    if filter_value != 'None':
+        filtered_pets = [item for item in pets if filter_value.lower() in item['name'].lower()]
+    else:
+        filtered_pets = pets
+    if currentPet:
+        filtered_pets = [item for item in filtered_pets if currentPet['name'] != item['name']]
+    return jsonify(filtered_list=filtered_pets, currentPet=currentPet)
+
+## return pets
+@app.route('/returnpet')
+def returnPet():
+    username = session.get('username', None)
+    session['current_pet'] = None
+    pets = get_db_pet()
+    return render_template('user.html', username=username, currentPet=None, pets=pets)
+    
+
 
 ## logout function
 @app.route('/logout')
 def logout():
-    session.pop('user', None)
+    session.clear()
     return redirect('/')
 
 if __name__ == "__main__":
@@ -79,3 +126,5 @@ if __name__ == "__main__":
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host="127.0.0.1", port=8080, debug=True)
+
+
