cc51d47ef57536a76dfb2fafb7c2ec77970a4032
graydeec <graydeec21@gmail.com>
2023-12-08 19:14:25 -0500

    Merge the sql file.

---
+++ populate_DB.sql
@@ -0,0 +1,56 @@
+use ROBOPETY;
+
+
+-- insert the robots into the database
+-- the robots will have a name and a photo_url
+-- the photo_url will be a link to the image in the cloud storage
+INSERT INTO robots (name, photo_url) VALUES ('azumarill', 'https://storage.cloud.google.com/robo_images/azumarill.png');
+INSERT INTO robots (name, photo_url) VALUES ('alcremie', 'https://storage.cloud.google.com/robo_images/alcremie.png');
+INSERT INTO robots (name, photo_url) VALUES ('armarouge', 'https://storage.cloud.google.com/robo_images/armarouge.png');
+INSERT INTO robots (name, photo_url) VALUES ('blaziken', 'https://storage.cloud.google.com/robo_images/blaziken.png');
+INSERT INTO robots (name, photo_url) VALUES ('blipbug', 'https://storage.cloud.google.com/robo_images/blipbug.png');
+INSERT INTO robots (name, photo_url) VALUES ('bramblin', 'https://storage.cloud.google.com/robo_images/bramblin.png');
+INSERT INTO robots (name, photo_url) VALUES ('breloom', 'https://storage.cloud.google.com/robo_images/breloom.png');
+INSERT INTO robots (name, photo_url) VALUES ('camerupt', 'https://storage.cloud.google.com/robo_images/camerupt.png');
+INSERT INTO robots (name, photo_url) VALUES ('charmander', 'https://storage.cloud.google.com/robo_images/charmander.png');
+INSERT INTO robots (name, photo_url) VALUES ('cottonee', 'https://storage.cloud.google.com/robo_images/cottonee.png');
+INSERT INTO robots (name, photo_url) VALUES ('croconaw', 'https://storage.cloud.google.com/robo_images/croconaw.png');
+INSERT INTO robots (name, photo_url) VALUES ('donphan', 'https://storage.cloud.google.com/robo_images/donphan.png');
+INSERT INTO robots (name, photo_url) VALUES ('dragalge', 'https://storage.cloud.google.com/robo_images/dragalge.png');
+INSERT INTO robots (name, photo_url) VALUES ('dwebble', 'https://storage.cloud.google.com/robo_images/dwebble.png');
+INSERT INTO robots (name, photo_url) VALUES ('escavalier', 'https://storage.cloud.google.com/robo_images/escavalier.png');
+INSERT INTO robots (name, photo_url) VALUES ('finizen', 'https://storage.cloud.google.com/robo_images/finizen.png');
+INSERT INTO robots (name, photo_url) VALUES ('gardevoir', 'https://storage.cloud.google.com/robo_images/gardevoir.png');
+INSERT INTO robots (name, photo_url) VALUES ('geodude', 'https://storage.cloud.google.com/robo_images/geodude.png');
+INSERT INTO robots (name, photo_url) VALUES ('groudon', 'https://storage.cloud.google.com/robo_images/groudon.png');
+INSERT INTO robots (name, photo_url) VALUES ('gulpin', 'https://storage.cloud.google.com/robo_images/gulpin.png');
+INSERT INTO robots (name, photo_url) VALUES ('hippowdon', 'https://storage.cloud.google.com/robo_images/hippowdon.png');
+INSERT INTO robots (name, photo_url) VALUES ('igglybuff', 'https://storage.cloud.google.com/robo_images/igglybuff.png');
+INSERT INTO robots (name, photo_url) VALUES ('iron_thorns', 'https://storage.cloud.google.com/robo_images/iron_thorns.png');
+INSERT INTO robots (name, photo_url) VALUES ('kilowattrel', 'https://storage.cloud.google.com/robo_images/kilowattrel.png');
+INSERT INTO robots (name, photo_url) VALUES ('kricketot', 'https://storage.cloud.google.com/robo_images/kricketot.png');
+INSERT INTO robots (name, photo_url) VALUES ('lampent', 'https://storage.cloud.google.com/robo_images/lampent.png');
+INSERT INTO robots (name, photo_url) VALUES ('latias', 'https://storage.cloud.google.com/robo_images/latias.png');
+INSERT INTO robots (name, photo_url) VALUES ('latios', 'https://storage.cloud.google.com/robo_images/latios.png');
+INSERT INTO robots (name, photo_url) VALUES ('leavanny', 'https://storage.cloud.google.com/robo_images/leavanny.png');
+INSERT INTO robots (name, photo_url) VALUES ('linoone', 'https://storage.cloud.google.com/robo_images/linoone.png');
+INSERT INTO robots (name, photo_url) VALUES ('pichu', 'https://storage.cloud.google.com/robo_images/pichu.png');
+INSERT INTO robots (name, photo_url) VALUES ('pignite', 'https://storage.cloud.google.com/robo_images/pignite.png');
+INSERT INTO robots (name, photo_url) VALUES ('primarina', 'https://storage.cloud.google.com/robo_images/primarina.png');
+INSERT INTO robots (name, photo_url) VALUES ('psyduck', 'https://storage.cloud.google.com/robo_images/psyduck.png');
+INSERT INTO robots (name, photo_url) VALUES ('revavroom', 'https://storage.cloud.google.com/robo_images/revavroom.png');
+INSERT INTO robots (name, photo_url) VALUES ('rotom', 'https://storage.cloud.google.com/robo_images/rotom.png');
+INSERT INTO robots (name, photo_url) VALUES ('salazzle', 'https://storage.cloud.google.com/robo_images/salazzle.png');
+INSERT INTO robots (name, photo_url) VALUES ('scream_tail', 'https://storage.cloud.google.com/robo_images/scream_tail.png');
+INSERT INTO robots (name, photo_url) VALUES ('seadra', 'https://storage.cloud.google.com/robo_images/seadra.png');
+INSERT INTO robots (name, photo_url) VALUES ('sewaddle', 'https://storage.cloud.google.com/robo_images/sewaddle.png');
+INSERT INTO robots (name, photo_url) VALUES ('shiinotic', 'https://storage.cloud.google.com/robo_images/shiinotic.png');
+INSERT INTO robots (name, photo_url) VALUES ('slaking', 'https://storage.cloud.google.com/robo_images/slaking.png');
+INSERT INTO robots (name, photo_url) VALUES ('staravia', 'https://storage.cloud.google.com/robo_images/staravia.png');
+INSERT INTO robots (name, photo_url) VALUES ('swanna', 'https://storage.cloud.google.com/robo_images/swanna.png');
+INSERT INTO robots (name, photo_url) VALUES ('totodile', 'https://storage.cloud.google.com/robo_images/totodile.png');
+INSERT INTO robots (name, photo_url) VALUES ('tynamo', 'https://storage.cloud.google.com/robo_images/tynamo.png');
+INSERT INTO robots (name, photo_url) VALUES ('urshifu', 'https://storage.cloud.google.com/robo_images/urshifu.png');
+INSERT INTO robots (name, photo_url) VALUES ('vanilluxe', 'https://storage.cloud.google.com/robo_images/vanilluxe.png');
+INSERT INTO robots (name, photo_url) VALUES ('vivillon', 'https://storage.cloud.google.com/robo_images/vivillon.png');
+INSERT INTO robots (name, photo_url) VALUES ('yveltal', 'https://storage.cloud.google.com/robo_images/yveltal.png');
